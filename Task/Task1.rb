b65cef1cd85351ac408ff9611672567dd2da8e9a
Aleksuk <aleksuk.m@gmail.com>
2015-01-30 00:38:53 +0200

    Renemade some methods

--- Task/Task1.rb
+++ Task/Task1.rb
@@ -38,8 +38,6 @@ def detect arr
 end
 
 def count arr, &block
-  return nil unless block_given?
-
   filtered_elements = filter arr, &block
   filtered_elements.length
 end	
@@ -57,7 +55,7 @@ def any? arr
   false
 end
 
-def every? arr
+def all? arr
   return false unless block_given?
 
   i = arr.length - 1
@@ -83,7 +81,7 @@ def reduce sum, arr
   sum
 end
 
-def interview_task *args
+def get_the_sum_of_the_squares_of_large_numbers *args
   min_index = args.find_index { |el| el == args.min }
 
   reduce 0, args do |sum, el, i|
@@ -104,51 +102,3 @@ def get_fibbonacci_by_index index
 
   fibbonacci_series[index]
 end
-
-p '#map'
-test = map [1,2,3,4] do |el|
-  el * 2
-end
-p test
-
-p '#filter'
-test = filter [1,2,3,4] do |el|
-  el % 2 == 0
-end
-p test
-
-p '#detect'
-test = detect [1,2,3] do |el|
-  el > 1
-end
-p test
-
-p '#count'
-test = count [1,2,3,4] do |el|
-  el % 3 == 0
-end
-p test
-
-p '#any?'
-test = any? [1,2,3,4] do |el|
-  el == 3
-end
-p test
-
-p '#every?'
-test = every? [1,2,3,4] do |el|
-  el > 0
-end
-p test
-
-p '#reduce'
-test = reduce 0, [1,2,3,4] do |sum, el|
-  sum + el
-end
-p test
-
-p '#interview_task'
-p interview_task 2, 1, 3
-
-p '#get_fibbonacci_by_index'
-p get_fibbonacci_by_index 10
